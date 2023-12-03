455614bffa56c3b13cab1537a9f4466cddf48b3b
hdaleee <hdaleee@kakao.com>
2023-12-03 22:07:13 +0900

    fix Transient props error in styled-component

--- src/components/Spacing/Spacing.tsx
+++ src/components/Spacing/Spacing.tsx
@@ -14,14 +14,14 @@ function Spacing({ overrideCss, size = 'spacing_d', spacingType = 'horizontal', 
   const spacingSize = useRem ? (`spacing_${size.charAt(size.length - 1).toUpperCase()}` as SpacingSizeType) : size;
   return (
     <ThemeProvider theme={ldsTheme}>
-      <StyledSpacing size={spacingSize} style={overrideCss} spacingType={spacingType} />
+      <StyledSpacing size={spacingSize} style={overrideCss} $spacingType={spacingType} />
     </ThemeProvider>
   );
 }
 
-const StyledSpacing = styled.div<{ size: string; spacingType: SpacingType }>`
-  ${({ spacingType }) => {
-    return { vertical: SpacingStyleCss.VerticalTypeCss, horizontal: SpacingStyleCss.HorizontalTypeCss }[spacingType];
+const StyledSpacing = styled.div<{ size: string; $spacingType: SpacingType }>`
+  ${({ $spacingType }) => {
+    return { vertical: SpacingStyleCss.VerticalTypeCss, horizontal: SpacingStyleCss.HorizontalTypeCss }[$spacingType];
   }};
 `;
 
