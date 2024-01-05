9046284b4313f14819499e60bb9ebb7eb6197159
hdaleee <hdaleee@kakao.com>
2024-01-05 19:43:25 +0900

    export Icon

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 import { Dropdown } from './Dropdown';
+import { Icon } from './Icon';
 import { Image } from './Image';
 import { Spacing } from './Spacing';
 import { Switch } from './Switch';
 import { Text } from './Text';
 import { VideoPlayer } from './VideoPlayer';
 
-export { Dropdown, Image, Spacing, Switch, Text, VideoPlayer };
+export { Dropdown, Icon, Image, Spacing, Switch, Text, VideoPlayer };
